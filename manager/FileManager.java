a520947e87b289436bdc6363069967be3f9a622f
Hannah <lilypickle@hotmail.fr>
2021-01-27 23:21:16 +0100

    continues tp7

--- manager/FileManager.java
+++ manager/FileManager.java
@@ -1,7 +1,10 @@
 package com.tactfactory.javaniveau2.tps.tp7.manager;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,20 +13,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
-public class FileManager {
+public class FileManager{
 
 //	./test/src/main/java/com/tactfactory/myproject/Myproject.java
 //	
-//	public FileManager(file) {
-//		this.file = file;
-//	}
-//	
-	public void createProjectstructure() {
+	
+	private File f;
+	
+	public FileManager(File Newfile){
+		this.f = Newfile;
+	}
+	
+	public void createProjectStructure() throws IOException {
 	
 		try { 
-	        File f = new File("D:\\MyProject.java");
+	        File fjava = new File("MyProject.java");
 	
-	        if (f.createNewFile())
+	        if (fjava.createNewFile())
 	            System.out.println("File created");
 	        else
 	            System.out.println("File already exists");
@@ -32,21 +38,45 @@ public class FileManager {
 	        System.err.println(e);
 	    }
 		
-		Path path = Paths.get("D:\\MyProject.java");
-		try {
-			String str = "package com.tactfactory.myproject;\n" +
-						"\n" +
-						"public class MyProject {\n" +
-						"\n" +
-						"  public static void main(String[] args) {\n" +
-						"    System.out.println(\"Hello world\");\n" +
-						"  }\n" +
-						"}\n";
-			byte[] bs = str.getBytes();
-			Path writtenFilePath = Files.write(path, bs);
-			System.out.println("Written content in file:\n"+ new String(Files.readAllBytes(writtenFilePath)));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		Path path = Paths.get(f);
+//		try {
+//			String str = "package com.tactfactory.myproject;\n" +
+//						"\n" +
+//						"public class MyProject {\n" +
+//						"\n" +
+//						"  public static void main(String[] args) {\n" +
+//						"    System.out.println(\"Hello world\");\n" +
+//						"  }\n" +
+//						"}\n";
+//			byte[] bs = str.getBytes();
+//			Path writtenFilePath = Files.write(path, bs);
+//			System.out.println("Written content in file:\n"+ new String(Files.readAllBytes(writtenFilePath)));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		
+		    String value = "package com.tactfactory.myproject;\n" +
+					"\n" +
+					"public class MyProject {\n" +
+					"\n" +
+					"  public static void main(String[] args) {\n" +
+					"    System.out.println(\"Hello world\");\n" +
+					"  }\n" +
+					"}\n";
+		    FileOutputStream fos = new FileOutputStream(f);
+		    DataOutputStream outStream = new DataOutputStream(new BufferedOutputStream(fos));
+		    outStream.writeUTF(value);
+		    outStream.close();
+
+	}
+
+	public void listFiles() {
+		// TODO Auto-generated method stub
+		String[] pathnames;
+		pathnames = f.list();
+		for (String pathname : pathnames) {
+            System.out.println(pathname);
+        }
+		
 	}
 }
